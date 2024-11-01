ccbbdaf86197b874a9f4434019bb5372ba54ffb8
Yiteng Peng <ypengbp@connect.ust.hk>
2024-11-01 22:20:21 +0800

    update

---
+++ mainpage/publications.js
@@ -0,0 +1,27 @@
+var publication_lists = [
+    { content: "MTZK: Testing and Exploring Bugs in Zero-Knowledge (ZK) Compilers", 
+        author: "Dongwei Xiao, Zhibo Liu, <ohln>Yiteng Peng</ohln>, and Shuai Wang.",
+        conf: "NDSS 2025" },
+    { content: "Testing and Understanding Deviation Behaviors in FHE-hardened Machine Learning Models.", 
+        author: "<ohln>Yiteng Peng</ohln>, Daoyuan Wu, Zhibo Liu, Dongwei Xiao, Zhenlan Ji, Juergen Rahmel, and Shuai Wang.",
+        conf: "ICSE 2025" },
+];
+
+// 获取包含这些内容的容器元素
+var container = document.getElementById("pubs_end");
+
+// 反转数组并遍历，实现倒序遍历
+publication_lists.reverse().forEach(function(item) {
+    var html = `
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-12">
+                <div class="atf-about-content atf-about-text atf-main-about wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.2s" data-wow-offset="0">
+                    <h3>${item.content}</h3>
+                    <h5>${item.author} <span class="gray-background">${item.conf}</span></h5>
+                </div>
+            </div>
+        </div>
+    `;
+    
+    container.insertAdjacentHTML('beforebegin', html);
+});
